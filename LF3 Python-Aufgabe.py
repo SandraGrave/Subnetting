fbc52b9f6aeb37933b2f48fd5499c1877ccb8a64
Sanny <s.grave@varengold.de>
2023-02-15 13:52:57 +0100

    test

---
+++ LF3 Python-Aufgabe.py
@@ -0,0 +1,10 @@
+wiederholen = True
+
+while (wiederholen == True):
+    netzwerkAdresseBasisnetz =
+    AnzSubnetz =                    ## Anzahl der gewünschten Subnetze
+    ## Zerlege Netzadresse in Array NetzIP
+    ## LogAnzSubnetz = log2(AnzSubnetz)
+
+    if (LogAnzSubnetz mod 1) == 0):
+        
