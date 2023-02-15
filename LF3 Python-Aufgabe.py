f079c9cae196376206584b5c029358de397ebc7c
Sanny <s.grave@varengold.de>
2023-02-15 15:44:55 +0100

    Struktur aufgeschrieben

--- LF3 Python-Aufgabe.py
+++ LF3 Python-Aufgabe.py
@@ -2,9 +2,21 @@ wiederholen = True
 
 while (wiederholen == True):
     netzwerkAdresseBasisnetz =
-    AnzSubnetz =                    ## Anzahl der gewünschten Subnetze
-    ## Zerlege Netzadresse in Array NetzIP
-    ## LogAnzSubnetz = log2(AnzSubnetz)
+    AnzSubnetz =                    # Anzahl der gewünschten Subnetze
+    # Zerlege Netzadresse in Array NetzIP
+    # LogAnzSubnetz = log2(AnzSubnetz)
 
-    if (LogAnzSubnetz mod 1) == 0):
-        
+        if (LogAnzSubnetz mod 1) == 0:
+            continue
+        else: LogAnzSubnetz = int(LogAnzSubnetz)+1
+
+    HostAnzSubnetz = 2^(8-LogAnzSubnetz)
+    # A: Anzahl der möglichen Hosts pro Netz HostAnzSubnetz-2
+    # A: Subnetzmaske für die Netze: ??? (selbst berechnen)
+
+    for i = 0; i < AnzSubnetze; i++:
+        # A: Netz i : NetzIP[0] + NetzIP[1] + NetzIP[2] + NetzIP[3] + i * HostAnzSubnetz)
+
+    # erneute Berechnung? Eingabe
+    # Eingabe Ja : Wiederholen =True
+    # Eingabe Nein: Wiederholen = False
