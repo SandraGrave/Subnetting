63a10fc373dc2ab741f45cfdc47051cd8c960f7b
Sanny <s.grave@varengold.de>
2023-03-22 10:29:36 +0100

    Zusatzaufgabe funktionsfähig, Tests fortgeführt

--- TestCases.py
+++ TestCases.py
@@ -19,6 +19,7 @@ class TestCases(unittest.TestCase):
             main.eingabe_basisnetz()
             self.assertEqual(mock_stdout.getvalue(), "Fehlerhafte Eingabe\n")
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_wenn_eingabe_der_subnetz_maske_unter_8(self, mock_stdout):
         with mock.patch('builtins.input', side_effect=["7", "8"]):                                        # wird sonst endlos ausgeführt, daher noch eine gültige Eingabe
@@ -31,8 +32,7 @@ class TestCases(unittest.TestCase):
             main.eingabe_der_subnetz_maske()
             self.assertEqual(mock_stdout.getvalue(), "Bei über 29 Subnetzen können keine Hosts mehr vergeben werden, bitte gib eine geringe Anzahl an\n")
 
-
-# Anzahl der Subnetzmaske: Eingabe und Prüfung (es darf kein Text eingegeben werden und nicht zu Hohe Anzahl der Subnetze)
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_wenn_eingabe_anz_subnetzmaske_richtig_dann_return_anz_subnetz(self):
         with mock.patch('builtins.input',  return_value="50"):
             freie_subnetz_stellen = 8
@@ -59,7 +59,7 @@ class TestCases(unittest.TestCase):
             main.eingabe_anz_subnetzmaske(freie_subnetz_stellen)
             self.assertEqual(mock_stdout.getvalue(), "Fehlerhafte Eingabe\n")
 
-
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_validiere_zahl_zwischen_ist_gueltige_eingabe(self):
         with mock.patch('builtins.input', return_value="12"):
             min = 2
@@ -68,7 +68,6 @@ class TestCases(unittest.TestCase):
             fehler_max = "Fehlermeldung Max"
             frage = "Wie viele Bits sind als Netzwerkpart gewünscht? (min 8, max 29): "
             assert main.validiere_zahl_zwischen(min, max, fehler_min, fehler_max, frage) == 12
-
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_wenn_validiere_zahl_zwischen_ist_keine_Zahl(self, mock_stdout):
@@ -103,15 +102,17 @@ class TestCases(unittest.TestCase):
             main.validiere_zahl_zwischen(min, max, fehler_min, fehler_max, frage)
             self.assertEqual(mock_stdout.getvalue(), "Fehlermeldung Max\n")
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_teile_netzip_als_array(self):
         netzadresse_basisnetz = "1.2.3.0"
         self.assertEqual(main.teile_netzip_als_array(netzadresse_basisnetz), ['1', '2', '3', '0'])
 
-
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_wenn_berechne_bits_aus_hostpart_return_ergebnis(self):
         anz_subnetz_as_int = 4
         self.assertEqual(main.berechne_bits_aus_hostpart(anz_subnetz_as_int), 2)
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_wenn_berechnung_anz_subnetze_hat_rest_dann_erhoehe_wert_und_gib_anz_subnetze_zurueck(self):
         log_anz_subnetz = 2.32
         self.assertEqual(main.berechnung_anz_subnetze(log_anz_subnetz), 3)
@@ -124,19 +125,20 @@ class TestCases(unittest.TestCase):
         log_anz_subnetz = 3
         self.assertEqual(main.berechnung_anz_subnetze(log_anz_subnetz), 3)
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_wenn_berechnung_anz_host_korrekt_gebe_anzahl_zurueck(self):     # (2 ^ (anz_bits_subnetz - log_anz_subnetz) - 2
         log_anz_subnetz = 5
         anz_bits_subnetz = 8
         self.assertEqual(main.berechnung_anz_host(log_anz_subnetz, anz_bits_subnetz), 6)
 
-
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_wenn_ausgabe_anz_host_gebe_anz_hosts_mit_Einleiungssatz_aus(self, mock_stdout):
         anz_hosts = 30
         main.ausgabe_anz_host(anz_hosts)
         self.assertEqual("\nAnzahl der Hosts pro Subnetz: 30\n", mock_stdout.getvalue())
 
-
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_wenn_berechnung_subnetzmaske_mit_22_freien_und_6_belegten_stellen_gebe_subnetz_maske_zurueck(self):
         anz_bits_subnetzpart = 6
         freie_bits_hostpart = 22
@@ -157,27 +159,57 @@ class TestCases(unittest.TestCase):
         freie_bits_hostpart = 24
         self.assertEqual(main.berechnung_subnetzmaske(anz_bits_subnetzpart, freie_bits_hostpart), 0b11111111000000000000000000000000)   # = 11111111 00000000 00000000 00000000
 
-    def test_umwandlung_int_32_zu_subnetzmaske(self):
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def test_wenn_umwandlung_int_32_zu_subnetzmaske_gueltige_eingabe_gebe_subnetzmaske_zurueck(self):
         subnetzmaske = 0b11111111000000000000000000000000           # = 11111111 00000000 00000000 00000000
         self.assertEqual(main.umwandlung_int_32_zu_subnetzmaske(subnetzmaske), "255.0.0.0")
 
+    def test_wenn_umwandlung_int_32_zu_subnetzmaske_eingabe_minimum_gebe_subnetzmaske_zurueck(self):
+        subnetzmaske = 0b00000000000000000000000000000000           # = 00000000 00000000 00000000 00000000
+        self.assertEqual(main.umwandlung_int_32_zu_subnetzmaske(subnetzmaske), "0.0.0.0")
 
+    def test_wenn_umwandlung_int_32_zu_subnetzmaske_eingabe_maximum_gebe_subnetzmaske_zurueck(self):
+        subnetzmaske = 0b11111111111111111111111111111111           # = 11111111 11111111 11111111 11111111
+        self.assertEqual(main.umwandlung_int_32_zu_subnetzmaske(subnetzmaske), "255.255.255.255")
+
+    def test_wenn_umwandlung_int_32_zu_subnetzmaske_eingabe_keine_32_bit_zahl_gebe_fehler(self): # 34 Bit-Binär-Zahl läuft durch, sollte doch eigentlich nicht?
+        subnetzmaske = 0b1111111111111111111111111111111111           # = 11111111 11111111 11111111 11111111
+        self.assertEqual(main.umwandlung_int_32_zu_subnetzmaske(subnetzmaske), "255.255.255.255")
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_wenn_ausgabe_subnetzmaske_gebe_subnetzmaske_mit_einleitungssatz_aus(self, mock_stdout):
         subnetzmaske_as_string = "255.255.255.0"
         main.ausgabe_subnetzmaske(subnetzmaske_as_string)
         self.assertEqual("Subnetzmaske: 255.255.255.0\n\n", mock_stdout.getvalue())
 
-    def test_umwandlung_subnetzmaske_zu_int_32(self):
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    def test_umwandlung_subnetzmaske_zu_int_32(self): # 1x 0.0.0.0, 255.255.255.255
         netz_ip =['255', '255', '128', '0']
-        self.assertEqual(main.umwandlung_subnetzmaske_zu_int_32(netz_ip), 0b11111111111111111000000000000000)   # = 11111111 11111111 10000000 00000000
+        self.assertEqual(main.umwandlung_subnetzmaske_zu_int_32(netz_ip), 0b11111111111111111000000000000000)           # = 11111111 11111111 10000000 00000000
 
+    def test_umwandlung_subnetzmaske_zu_int_32(self):
+        netz_ip =['255', '255', '255', '255']
+        self.assertEqual(main.umwandlung_subnetzmaske_zu_int_32(netz_ip), 0b11111111111111111111111111111111)           # = 11111111 11111111 11111111 11111111
 
-    def test_erstelle_bitmaske_netzwerkpart(self):  # Anzahl der Bits für Netzwerkpart, 1en werden gesetzt
+    def test_umwandlung_subnetzmaske_zu_int_32(self):
+        netz_ip =['0', '0', '0', '0']
+        self.assertEqual(main.umwandlung_subnetzmaske_zu_int_32(netz_ip), 0b00000000000000000000000000000000)           # = 00000000 00000000 00000000 00000000
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    def test_wenn_erstelle_bitmaske_netzwerkpart_eingabe_ist_gueltig_gebe_subnetzmaske_zurueck(self):  # Anzahl der Bits für Netzwerkpart, 1en werden gesetzt
         freie_bits_hostpart = 8
-        self.assertEqual(main.erstelle_bitmaske_netzwerkpart(freie_bits_hostpart), 0b11111111111111111111111100000000)  # = 11111111 1111111 111111111 00000000
+        self.assertEqual(main.erstelle_bitmaske_netzwerkpart(freie_bits_hostpart), 0b11111111111111111111111100000000)  # = 11111111 11111111 11111111 00000000
 
+    def test_wenn_erstelle_bitmaske_netzwerkpart_keine_freien_bits_fuer_hostpart_gebe_subnetzmaske_zurueck(self):
+        freie_bits_hostpart = 0
+        self.assertEqual(main.erstelle_bitmaske_netzwerkpart(freie_bits_hostpart), 0b11111111111111111111111111111111)  # = 11111111 11111111 11111111 11111111
 
+    def test_wenn_erstelle_bitmaske_netzwerkpart_freien_bits_fuer_hostpart_ist_eine_ungerade_zahl_gebe_subnetzmaske_zurueck(self):
+        freie_bits_hostpart = 11
+        self.assertEqual(main.erstelle_bitmaske_netzwerkpart(freie_bits_hostpart), 0b11111111111111111111100000000000)  # = 11111111 11111111 11111000 00000000
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_berechnung_netzwerk_broadcast(self):
         with mock.patch('main.erstelle_bitmaske_netzwerkpart', side_effect=[0b11111111111111111111111100000000]), \
                 mock.patch('main.berechnung_netzwerk_adresse', side_effect=["255.128.3.0", "255.128.3.128"]), \
@@ -187,27 +219,26 @@ class TestCases(unittest.TestCase):
             freie_bits_hostpart = 8
             self.assertEqual(main.berechnung_netzwerk_broadcast(anz_subnetz, netz_ip_as_int, freie_bits_hostpart), [['255.128.3.0', '255.128.3.128'], ['255.128.3.127', '255.128.3.255']])
 
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_wenn_ausgabe_netzwerk_broadcast_gebe_beides_aus(self, mock_stdout):
+        netzwerkadressen_liste = [['255.0.0.0', '255.8.0.0', '255.16.0.0', '255.24.0.0'], ['255.7.255.255', '255.15.255.255', '255.23.255.255', '255.31.255.255']]
+        main.ausgabe_netzwerk_broadcast(netzwerkadressen_liste)
+        self.assertEqual("1. Subnetz:\nNetzwerkadresse: 255.0.0.0\nBroadcastadresse: 255.7.255.255\n\n"
+                         "2. Subnetz:\nNetzwerkadresse: 255.8.0.0\nBroadcastadresse: 255.15.255.255\n\n"
+                         "3. Subnetz:\nNetzwerkadresse: 255.16.0.0\nBroadcastadresse: 255.23.255.255\n\n"
+                         "4. Subnetz:\nNetzwerkadresse: 255.24.0.0\nBroadcastadresse: 255.31.255.255\n\n", mock_stdout.getvalue())
 
-    #@unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    #def test_wenn_ausgabe_netzwerk_broadcast_gebe_beides_aus(self, mock_stdout):
-    #    netzwerkadressen_liste = [['255.0.0.0', '255.8.0.0', '255.16.0.0', '255.24.0.0']]
-    #    main.ausgabe_netzwerk_broadcast(netzwerkadressen_liste)
-    #    self.assertEqual("1. Subnetz:\nNetzwerkadresse: 255.0.0.0\nBroadcastadresse: 255.7.255.255\n\n"
-    #                     "2. Subnetz:\nNetzwerkadresse: 255.8.0.0\nBroadcastadresse: 255.15.255.255\n\n"
-    #                     "3. Subnetz:\nNetzwerkadresse: 255.16.0.0\nBroadcastadresse: 255.23.255.255\n\n"
-    #                     "4. Subnetz:\nNetzwerkadresse: 255.24.0.0\nBroadcastadresse: 255.31.255.255\n\n", mock_stdout.getvalue())
-
-# '255.32.0.0', '255.40.0.0', '255.48.0.0', '255.56.0.0', '255.64.0.0', '255.72.0.0', '255.80.0.0', '255.88.0.0', '255.96.0.0', '255.104.0.0', '255.112.0.0', '255.120.0.0', '255.128.0.0', '255.136.0.0', '255.144.0.0', '255.152.0.0', '255.160.0.0', '255.168.0.0', '255.176.0.0', '255.184.0.0', '255.192.0.0', '255.200.0.0', '255.208.0.0', '255.216.0.0', '255.224.0.0', '255.232.0.0', '255.240.0.0', '255.248.0.0'], ['255.7.255.255', '255.15.255.255', '255.23.255.255', '255.31.255.255', '255.39.255.255', '255.47.255.255', '255.55.255.255', '255.63.255.255', '255.71.255.255', '255.79.255.255', '255.87.255.255', '255.95.255.255', '255.103.255.255', '255.111.255.255', '255.119.255.255', '255.127.255.255', '255.135.255.255', '255.143.255.255', '255.151.255.255', '255.159.255.255', '255.167.255.255', '255.175.255.255', '255.183.255.255', '255.191.255.255', '255.199.255.255', '255.207.255.255', '255.215.255.255', '255.223.255.255', '255.231.255.255', '255.239.255.255', '255.247.255.255', '255.255.255.255'
-
-
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     def test_wenn_erneute_berechnung_abfrage_ja_return_true(self):
         with mock.patch('builtins.input', return_value="Ja"):
             assert main.erneute_berechnung_abfrage() == True
 
-
     def test_wenn_erneute_berechnung_abfrage_nein_return_false(self):
         with mock.patch('builtins.input', return_value="Nein"):
             assert main.erneute_berechnung_abfrage() == False
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     # Main-Methode testen?
 
